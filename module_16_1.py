330d8243cf6824b1dcd2f73d66e04da4779d3e65
Olganord <156664559+Olganord@users.noreply.github.com>
2025-02-06 19:58:12 +0500

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -1,19 +1,22 @@
 from fastapi import FastAPI
-
+# Создаем экземпляр приложения FastAPI
 app = FastAPI()
+# Определение базового маршрута
 
-@app.get('/')
-async def main_page():
-    return 'Главная страница'
+@app.get("/")
+async def  get_main_page():
+    return {"message": "Главная страница"}
 
-@app.get('/user/admin')
-async def admin_page():
-    return 'Вы вошли как администратор'
+@app.get("/user/admin")
+async def get_admin_page():
+    return {"message": "Вы вошли как администратор"}
 
-@app.get('/user/{user_id}')
-async def user(user_id: int):
-    return f'Вы вошли как пользователь №{user_id}'
+@app.get("/user/{user_id}")
+async def get_user_number():
+    return {"message": "Вы вошли как пользователь № 123"}
 
-@app.get('/user')
-async def info_user(user_name: str, user_age: int):
-    return f'Информация о пользователе. Имя: {user_name}, возраст: {user_age}'
+@app.get("/user")
+async def get_user_info():
+    return {"message": "Информация о пользователе. Имя: 'Ilya', возраст: 24"}
+
+# uvicorn module_16_1:app --reload
