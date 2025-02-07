feb8225893afa49e643cfbf1caebf9b06590196a
Olganord <156664559+Olganord@users.noreply.github.com>
2025-02-07 19:08:43 +0500

    Update module_16_2.py

--- module_16_2.py
+++ module_16_2.py
@@ -1,28 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from typing import Annotated
-from fastapi import Path
 
+# Создаем экземпляр приложения FastAPI
 app = FastAPI()
+# Определение базового маршрута
 
-@app.get('/')
-async def main_page():
-    return 'Главная страница'
-
-@app.get('/user/admin')
-async def admin_page():
-    return 'Вы вошли как администратор'
-
-# @app.get('/user/{user_id}')
-# async def user(user_id: int):
-#     return f'Вы вошли как пользователь №{user_id}'
+@app.get("/")
+async def  get_main_page():
+    # return {"message": "Главная страница"}
+    pass
+# @app.get("/user/admin")
+# async def get_admin_page():
+#     return {"message": "Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def get_user(user_id: Annotated[int, Path(ge=1, le=100,title="User ID", description="Enter User ID", example=1)]):
+async def get_user_by_id(user_id: Annotated[int, Path(ge=1, le=100, title="ID of the user", description="Enter User ID",
+                                                 example=1)]):
     return {"user_id": user_id}
 
 @app.get('/user/{username}/{age}')
-async def info_user(username: Annotated[str, Path(ge=5, le=20,  title='Enter username' , description="Enter username", example="UrbanUser")],
-                    age: Annotated[int, Path(ge=18, le=120,  title='Enter age'  , description="Enter age", example=24)]):
-        return f'Информация о пользователе. Имя: {username}, возраст: {age}'
+async def get_user_info(username: Annotated[str, Path(min_length=5, max_length=20,  title='Username' ,
+                                                      description="Enter username",
+                                                  example="UrbanUser")],
+                    age: Annotated[int, Path(ge=18, le=120,  title='Age'  , description="Enter age", example=24)]):
+        return {"username": username, "age": age}
+
+# @app.get("/user")
+# async def get_user_info():
+#     return {"message": "Информация о пользователе. Имя: 'Ilya', возраст: 24"}
+
 # uvicorn module_16_2:app --reload
-#
